98b90661e616526da4f553761800ee86de49a898
Rama Krishnar Muthaiah <ramakrishnarmuthaiah@gmail.com>
2021-07-26 00:14:47 +0530

    Dashboard navbar changes

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,60 @@
 import React from "react";
+import {BsFillAwardFill} from 'react-icons/bs';
 import './Dashboard.scss';
 export class Dashboard extends React.Component{
     
     render(){
         return (
-            <div className="dashboard-wrap">
-                <div className="navbar">
-                    Welcome to Dashboard Section
-                </div>
-            </div>
+            React.createElement(
+                "div",
+                {
+                    "className":'dashboard-wrap'
+                },
+               React.createElement(
+                   React.Fragment,
+                   null,
+                   React.createElement(
+                       "div",
+                       {
+                           "className":'navbar'
+                       },
+                       React.createElement(
+                           "div",
+                           {
+                               "className":'left'
+                           },
+                           <BsFillAwardFill size="30"/>
+                       ),
+                       React.createElement(
+                           "ul",
+                           {
+                               "className":'navlist'
+                           },
+                           React.createElement(
+                               "li",
+                               {
+                                   "className":''
+                               },
+                               "About"
+                           ),
+                           React.createElement(
+                            "li",
+                            {
+                                "className":''
+                            },
+                            "Home"
+                        ),
+                        React.createElement(
+                            "li",
+                            {
+                                "className":''
+                            },
+                            "Contact"
+                        )
+                       )
+                   )
+               )
+            )
         )
     }
 }
